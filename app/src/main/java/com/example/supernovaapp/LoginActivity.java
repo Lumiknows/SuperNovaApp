1d0680bdccc5aed56d734f932de05371ef8de622
Your Name <you@example.com>
2025-04-09 15:01:46 +0800

    commit pls pls pls

---
+++ app/src/main/java/com/example/supernovaapp/LoginActivity.java
@@ -0,0 +1,33 @@
+package com.example.supernovaapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    EditText username, password;
+    Button signInBtn, createAccountBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        signInBtn = findViewById(R.id.signInBtn);
+        createAccountBtn = findViewById(R.id.createAccountBtn);
+
+        signInBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginActivity.this, StoreActivity.class);
+            startActivity(intent);
+        });
+
+
+        createAccountBtn.setOnClickListener(v -> {
+            // Navigate to RegisterActivity
+        });
+    }
+}
---
+++ app/src/main/java/com/example/supernovaapp/StoreActivity.java
@@ -0,0 +1,12 @@
+package com.example.supernovaapp;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StoreActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_store);
+    }
+}
