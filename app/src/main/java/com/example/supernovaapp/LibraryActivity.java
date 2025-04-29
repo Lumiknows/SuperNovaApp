8f8d563a1376b81baa45052991c876890d80a03b
CLINT <148334301+fuzziiz@users.noreply.github.com.>
2025-04-29 22:32:52 +0800

    Added function to Store and Library button for both layout

--- app/src/main/java/com/example/supernovaapp/LibraryActivity.java
+++ app/src/main/java/com/example/supernovaapp/LibraryActivity.java
@@ -1,6 +1,10 @@
 package com.example.supernovaapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LibraryActivity extends AppCompatActivity {
@@ -8,5 +12,21 @@ public class LibraryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_library);
+
+        ImageButton storeButton = findViewById(R.id.store_button);
+        ImageButton backButton = findViewById(R.id.back_button);
+
+        View.OnClickListener goToStoreListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LibraryActivity.this, StoreActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+            }
+        };
+
+        // Since same man sila ug function gi usa nalang nako ang listener para dili kaayo taas ang code
+        storeButton.setOnClickListener(goToStoreListener);
+        backButton.setOnClickListener(goToStoreListener);
     }
 }
--- app/src/main/java/com/example/supernovaapp/StoreActivity.java
+++ app/src/main/java/com/example/supernovaapp/StoreActivity.java
@@ -1,6 +1,10 @@
 package com.example.supernovaapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class StoreActivity extends AppCompatActivity {
@@ -8,5 +12,15 @@ public class StoreActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store);
+
+        ImageButton libraryButton = findViewById(R.id.library_button);
+        libraryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StoreActivity.this, LibraryActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+            }
+        });
     }
 }
