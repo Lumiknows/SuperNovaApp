260b9486c259c8426b1fdf5dacd3e8deb4994587
CLINT <148334301+fuzziiz@users.noreply.github.com.>
2025-05-01 09:20:36 +0800

    Remove back button, maka navigate ra man balik sa store gamit ang tab layout

--- app/src/main/java/com/example/supernovaapp/LibraryFragment.java
+++ app/src/main/java/com/example/supernovaapp/LibraryFragment.java
@@ -1,12 +1,17 @@
 package com.example.supernovaapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -60,5 +65,10 @@ public class LibraryFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_library, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
     }
 }
