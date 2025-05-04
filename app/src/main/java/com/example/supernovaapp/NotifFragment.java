927113dbab779b95c91db0116a08290295a720a0
Lumiknows <neilandreibona@gmail.com>
2025-05-05 02:52:36 +0800

    monday commit time for rest haha yawa

--- app/src/main/java/com/example/supernovaapp/NotifFragment.java
+++ app/src/main/java/com/example/supernovaapp/NotifFragment.java
@@ -1,26 +1,18 @@
 package com.example.supernovaapp;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link NotifFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.fragment.app.Fragment;
+
 public class NotifFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -28,15 +20,6 @@ public class NotifFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment NotifFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static NotifFragment newInstance(String param1, String param2) {
         NotifFragment fragment = new NotifFragment();
         Bundle args = new Bundle();
@@ -59,6 +42,19 @@ public class NotifFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notif, container, false);
+        View view = inflater.inflate(R.layout.fragment_notif, container, false);  // Make sure 'fragment_notif.xml' exists
+
+        // Find the layout for Wukong (make sure it has android:id="@+id/wukong_layout" in fragment_notif.xml)
+        View wukongLayout = view.findViewById(R.id.wukong_layout);
+
+        // Set click listener to open GameView activity
+        wukongLayout.setOnClickListener(this::onClick);
+
+        return view;
+    }
+
+    private void onClick(View v) {
+        Intent intent = new Intent(getActivity(), GameView.class);  // Make sure GameView activity is in the manifest
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/supernovaapp/ProfilePage.java
+++ app/src/main/java/com/example/supernovaapp/ProfilePage.java
@@ -1,13 +1,28 @@
 package com.example.supernovaapp;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ProfilePage extends AppCompatActivity {
+
+    private ImageView backButton;  // Declare back button
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.profile_page);
+        setContentView(R.layout.profile_page);  // Make sure profile_page.xml is correct
+
+        // Initialize the back button
+        backButton = findViewById(R.id.back_button);  // Ensure this ID is correct in XML
+
+        // Set the back button to close the activity when clicked
+        backButton.setOnClickListener(v -> finish());
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();  // This will perform the default back button behavior
     }
 }
