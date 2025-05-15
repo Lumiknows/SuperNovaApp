300cd9eee13ce625496c04870d696df2f9b10f3a
CLINT <148334301+fuzziiz@users.noreply.github.com.>
2025-05-15 17:45:03 +0800

    dunno

--- app/src/main/java/com/example/supernovaapp/StoreFragment.java
+++ app/src/main/java/com/example/supernovaapp/StoreFragment.java
@@ -13,6 +13,7 @@ import androidx.viewpager2.widget.ViewPager2;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
